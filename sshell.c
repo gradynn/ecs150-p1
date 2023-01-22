f984d5fe67c3ffe51e771fc5e9df6c9c58c149bc
ChKeighley <123129785+ChKeighley@users.noreply.github.com>
2023-01-22 13:55:31 -0800

    Attempted struct

--- sshell.c
+++ sshell.c
@@ -2,16 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define CMDLINE_MAX 512
 
 int psuedo_system(char*, char*[]);
 
+struct Command {
+	char* whole_cmd;
+	char* cmd;
+	char* args[];
+};
+/*
+int command_parse(struct Command *command, char* whole_cmd)
+{
+	command->whole_cmd = whole_cmd;
+
+        command->cmd = strtok(whole_cmd, " ");
+        command->args = {NULL, NULL, NULL, NULL,
+ 		        NULL, NULL, NULL, NULL,
+                        NULL, NULL, NULL, NULL,
+                        NULL, NULL, NULL, NULL};
+         
+	  	command->args[0] = command->cmd;
+
+                char* token = strtok(NULL, " ");
+                int i = 1;
+		do {
+			if (i == 16)
+				return 1;
+                        command->args[i] = token;
+                        token = strtok(NULL, " ");
+                	i++;
+		} while (token != NULL);
+
+		return 0;
+}
+*/
 int main(void)
 {
         char cmd[CMDLINE_MAX];
 
         while (1) {
+		struct Command command;
                 char *nl;
                 int retval;
 
@@ -32,34 +65,50 @@ int main(void)
                 if (nl)
                         *nl = '\0';
 
+
+
                 /* Builtin command */
                 if (!strcmp(cmd, "exit")) {
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
 
-                /* Regular command */                
-                char input[CMDLINE_MAX];
-                strcpy(input, cmd);
-                char* command = strtok(input, " ");
-                char* args[16] = {NULL, NULL, NULL, NULL,
+                /* Regular command */
+		
+                strcpy(command.whole_cmd, cmd);
+		char* token = strtok(cmd, " ");
+
+              	strcpy(command.cmd, token);
+               /* command.args = {NULL, NULL, NULL, NULL,
                                  NULL, NULL, NULL, NULL,
                                  NULL, NULL, NULL, NULL,
                                  NULL, NULL, NULL, NULL};
-                args[0] = command;
+                */
+		strcpy(command.args[0], command.cmd);
 
-                char* token = strtok(NULL, " ");
+                token = strtok(NULL, " ");
                 int i = 1;
-                do {
-                        args[i] = token;
+		do {
+                        strcpy(command.args[i], token);
                         token = strtok(NULL, " ");
                         i++;
                 } while (token != NULL);
+/*
+		struct Command *command_ptr = &command;
+		int error = command_parse(command_ptr, cmd);
+		
+		if (error == 1)
+			fprintf(stderr, "Error: too many process arguments\n");
+*/		
+		//int arr_len = sizeof(command.args)/sizeof(command.args[0]);
 
+//		for (int i = 0; i < 2; i++)
+//			printf("%s\n", command.args[i]);
 
-                retval = psuedo_system(command, args);
+                retval = psuedo_system(command.cmd, command.args);
+//                printf("2 %s\n", cmd);
                 fprintf(stderr, "+ completed '%s' [%d]\n",
-                        cmd, retval);
+                        command.whole_cmd, retval);
         }
 
         return EXIT_SUCCESS;
@@ -68,14 +117,14 @@ int main(void)
 int psuedo_system(char* cmd, char* args[])
 {
         int pid = fork();
+	int status = 0;
+
         if (pid == 0) { // child
                 execvp(cmd, args);
         }
         else { // parent
-                int status;
                 waitpid(pid, &status, 0);
-                return WEXITSTATUS(status);
         }
+
+	return WEXITSTATUS(status);
 }
-
-
